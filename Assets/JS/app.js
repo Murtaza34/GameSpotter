9ccdd57df78c2fe0d5ee8a833e41e9df2d1a789f
seriksa3 <kellyseriksa7@gmail.com>
2023-02-15 00:50:04 +0100

    js for getting videos

--- Assets/JS/app.js
+++ Assets/JS/app.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
     const apiKey = 'f9ad2256413a41afa9dc99e837599c1b'
     var queryUrl = 'https://api.rawg.io/api/games?key='+apiKey;
-    var queryUrl1 = 'https://api.rawg.io/api/games/3498/movies?key='+apiKey;
     $.ajax({
         url:queryUrl,
         method:"GET"
@@ -12,11 +11,11 @@ $(document).ready(function(){
                 const dispImg = $('.game-cover');
                 dispImg[i].setAttribute('src',img);
             };
+            
         };
         function displayTitle(){
             for (let i = 0; i < 15; i++) {
                 var title = response.results[i].name;
-                console.log(title);
                 const dispTitle = $('.game-title');
                 dispTitle[i].setAttribute('h5',title);
                 dispTitle[i].append(title);
@@ -25,16 +24,36 @@ $(document).ready(function(){
         function displayYear(){
             for (let i = 0; i < 15; i++) {
                 var year = response.results[i].released;
-                console.log(year);
                 const dispYear = $('.game-year');
                 dispYear[i].setAttribute('p',year);
                 dispYear[i].append(year);
             };
         };
+        function getId() {
+            for (let i=0; i<15; i++){
+                var vidId = response.results[i].id;
+                console.log(vidId);
+                const queryUrl1 = 'https://api.rawg.io/api/games/'+vidId+'/movies?key='+apiKey;
+                $.ajax({
+                    url:queryUrl1,
+                    method:'GET'
+                }).then(function (response) {
+                    for (let i=0; i<vidId; i++){
+                        console.log(response.results[i]);
+                    };
+                });
+        };
+    
+            
+        };
         displayImage();
         displayTitle();
         displayYear();
-    }).catch(error)
+        console.log(response);
+        getId();
+        dispVid();
+    })
+    .catch(error)
     console.log(error);;
-
+    
 });
