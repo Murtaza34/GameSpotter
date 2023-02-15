72a4ada6ef59207f2f1cb433aa3eef65ab6105c9
unknown <you@example.com>
2023-02-15 15:38:39 +0000

    Modal testing

--- Assets/JS/app.js
+++ Assets/JS/app.js
@@ -1,40 +1,59 @@
-$(document).ready(function(){
-    const apiKey = 'f9ad2256413a41afa9dc99e837599c1b'
-    var queryUrl = 'https://api.rawg.io/api/games?key='+apiKey;
-    var queryUrl1 = 'https://api.rawg.io/api/games/3498/movies?key='+apiKey;
-    $.ajax({
-        url:queryUrl,
-        method:"GET"
-    }).then(function (response) {
-        function displayImage() {
-            for (let i = 0; i < 15; i++) {
-                var img = response.results[i].background_image;
-                const dispImg = $('.game-cover');
-                dispImg[i].setAttribute('src',img);
-            };
-        };
-        function displayTitle(){
-            for (let i = 0; i < 15; i++) {
-                var title = response.results[i].name;
-                console.log(title);
-                const dispTitle = $('.game-title');
-                dispTitle[i].setAttribute('h5',title);
-                dispTitle[i].append(title);
-            };
+$(document).ready(function () {
+  const apiKey = "f9ad2256413a41afa9dc99e837599c1b";
+  var queryUrl = "https://api.rawg.io/api/games?key=" + apiKey;
+  // var queryUrl1 = "https://api.rawg.io/api/games/3498/movies?key=" + apiKey;
+  $.ajax({
+    url: queryUrl,
+    method: "GET",
+  })
+    .then(function (response) {
+      function displayImage() {
+        for (let i = 0; i < 15; i++) {
+          var img = response.results[i].background_image;
+          const dispImg = $(".game-cover");
+          dispImg[i].setAttribute("src", img);
         }
-        function displayYear(){
-            for (let i = 0; i < 15; i++) {
-                var year = response.results[i].released;
-                console.log(year);
-                const dispYear = $('.game-year');
-                dispYear[i].setAttribute('p',year);
-                dispYear[i].append(year);
-            };
-        };
-        displayImage();
-        displayTitle();
-        displayYear();
-    }).catch(error)
-    console.log(error);;
+      }
+      function displayTitle() {
+        for (let i = 0; i < 15; i++) {
+          var title = response.results[i].name;
+          console.log(title);
+          const dispTitle = $(".game-title");
+          dispTitle[i].setAttribute("h5", title);
+          dispTitle[i].append(title);
+        }
+      }
+      function displayYear() {
+        for (let i = 0; i < 15; i++) {
+          var year = response.results[i].released;
+          console.log(year);
+          const dispYear = $(".game-year");
+          dispYear[i].setAttribute("p", year);
+          dispYear[i].append(year);
+        }
+      }
 
+      function getId() {
+        const vidId = "3498";
+        const queryUrl1 = "https://api.rawg.io/api/games/" + vidId + "/movies?key=" + apiKey;
+        $.ajax({
+          url: queryUrl1,
+          method: "GET",
+        }).then(function (response) {
+          console.log(response);
+          var videoGameId = response.results[0].data[480];
+          console.log('video link is:', videoGameId);
+          const gameVideo = $(".game-video");
+          var gvid = gameVideo.attr("src", videoGameId);
+          console.log('Video', gvid);
+          // gameVideo.append(videoGameId);
+        });
+      }
+      displayImage();
+      displayTitle();
+      displayYear();
+      getId();
+    })
+    .catch(error);
+  console.log(error);
 });
